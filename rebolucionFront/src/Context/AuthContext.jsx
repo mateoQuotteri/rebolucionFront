8e7ebf2f10c931ed0adb037ee40828ed38d85767
Mateo <quotterimateo@gmail.com>
2025-01-10 19:25:50 -0300

    cuando me logueo ese logueo se guarda en el localstorage eñ jwt y la data del usuario decodificada

--- rebolucionFront/src/Context/AuthContext.jsx
+++ rebolucionFront/src/Context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import {jwtDecode} from "jwt-decode";
 
 const AuthContext = createContext();
 
@@ -16,16 +17,27 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   // Guardar el estado del usuario en localStorage al iniciar sesión
-  const login = (userData) => {
-    setUser(userData);
-    setIsLoggedIn(true);
-    localStorage.setItem("user", JSON.stringify(userData));
+  const login = (jwt) => {
+    try {
+      // Decodificar el JWT
+      const decodedUser = jwtDecode(jwt.token);
+
+      // Guardar los datos decodificados en el estado y en localStorage
+      setUser(decodedUser);
+      setIsLoggedIn(true);
+
+      localStorage.setItem("jwt", jwt.token); // Guardar el JWT original
+      localStorage.setItem("user", JSON.stringify(decodedUser)); // Guardar datos del usuario
+    } catch (error) {
+      console.error("Error al decodificar el JWT:", error);
+    }
   };
 
   // Eliminar el estado del usuario de localStorage al cerrar sesión
   const logout = () => {
     setUser(null);
     setIsLoggedIn(false);
+    localStorage.removeItem("jwt");
     localStorage.removeItem("user");
   };
 
